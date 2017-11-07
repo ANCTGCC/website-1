f927add512efbabad8ec1ac0af58e9a61b0d923c
kuron334 <e1542@s.akashi.ac.jp>
2017-11-07 17:51:13 +0900

    5th

--- js/gcc.js
+++ js/gcc.js
@@ -65,16 +65,16 @@ $(function(){
     $("#jQueryPush").toggleClass("kaiten2");
   });
   $("#jQueryPush").one("click", function(){
-    $(".navbar li:not(.title):nth-child(1)").delay(500).animate({
+    $(".navbar li:nth-child(1)").delay(500).animate({
       opacity: 1,
     },1000);
-    $(".navbar li:not(.title):nth-child(2)").delay(1000).animate({
+    $(".navbar li:nth-child(2)").delay(1000).animate({
       opacity: 1,
     },1000);
-    $(".navbar li:not(.title):nth-child(3)").delay(1500).animate({
+    $(".navbar li:nth-child(3)").delay(1500).animate({
       opacity: 1,
     },1000);
-    $(".navbar li:not(.title):nth-child(4)").delay(2000).animate({
+    $(".navbar li:nth-child(4)").delay(2000).animate({
       opacity: 1,
     },1000);  
   });
